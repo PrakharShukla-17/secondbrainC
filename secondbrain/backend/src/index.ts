1a796522a3cb4f982f122e71c1f15cdf9fe34a3c
brownPenguin43 <rishishukla17122003@gmail.com>
2026-05-16 12:42:57 +0530

    updated cors thingy

--- secondbrain/backend/src/index.ts
+++ secondbrain/backend/src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://secondbrain-83dpklrhb-prakhar-shuklas-projects-408af39e.vercel.app' 
+    'https://secondbrain-c-xi.vercel.app' 
   ],
   credentials: true
 }));
